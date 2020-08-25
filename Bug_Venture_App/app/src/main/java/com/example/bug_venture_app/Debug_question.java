ff928412a5e1c3288158f2b2d60a27ee84cd26db
sahil0209 <61358568+sahil0209@users.noreply.github.com>
2020-08-25 12:39:12 +0530

    Adding the timer
    This is change is to add the timer in the app

--- Bug_Venture_App/app/src/main/java/com/example/bug_venture_app/Debug_question.java
+++ Bug_Venture_App/app/src/main/java/com/example/bug_venture_app/Debug_question.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class Debug_question extends AppCompatActivity {
 
     Button next;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class Debug_question extends AppCompatActivity {
         setContentView(R.layout.activity_debug_question);
 
         next = (Button) findViewById(R.id.button6);
+        textView2 = (TextView) findViewById(R.id.textView2);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,37 @@ public class Debug_question extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        new CountDownTimer(30500,1000) {
+            @Override
+            public void onTick(long l) {
+                if(l/1000<(long)11){
+                    textView2.setTextColor(Color.RED);
+                }
+                else if(l/1000<(long)21){
+                    textView2.setTextColor(Color.rgb(255,204,0));
+                }
+                if(l/1000<(long)10){
+                    textView2.setText("0"+String.valueOf(l/1000)+"s");
+                }
+                else{
+                    textView2.setText(String.valueOf(l/1000)+"s");
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+                // Also in place of rsAmount.getText.toString() => Change it according to the aswer given or not also if given what was given
+                if(rsAmount.getText().toString().equals("") || rsAmount.getText().toString().equals("0")){ // BY this if the user has given the wrong answer or not given any answer then he will be out
+                    Intent intent = new Intent(getApplicationContext(),Main2Activity.class); // Here change the activity name for the newQuestion 
+                    intent.putExtra("name","Your BugVenture Ends Here.");
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"You Answered a question",Toast.LENGTH_SHORT).show(); // If user answered the correct one direct it to the next question by using intent again
+                }
+            }
+        }.start();
     }
     @Override
     public void onBackPressed(){
